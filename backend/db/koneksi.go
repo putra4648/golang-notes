56fe45ea45cbd6c46bbfdcf4a8b6b7372d79a8ba
Putra <wakwaujaim@gmail.com>
2025-08-26 06:14:20 +0000

    feat: added docker

--- backend/db/koneksi.go
+++ backend/db/koneksi.go
@@ -1,13 +1,23 @@
 package db
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func DB() (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	name := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	url := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, name, port)
 	return gorm.Open((postgres.New(postgres.Config{
-		DSN:                  "host=localhost user=user password=user123 dbname=notes_db port=5432 sslmode=disable",
+		DSN:                  url,
 		PreferSimpleProtocol: true,
 	})), &gorm.Config{})
 }
